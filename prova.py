cc4f89fb5314b9c1618edddbbc82f0a40bd9d8c5
evila2 <144156105+alexandri9090@users.noreply.github.com>
2023-09-25 08:27:37 -0300

    Update prova.py

--- prova.py
+++ prova.py
@@ -2,3 +2,4 @@ v1=int(input("digite o ano atual:"))
 v2=int(input("digite sua data de nascimento:"))
 s=v1-v2
 print("você tem aproximadamente ",s,"anos")
+#observção na data de nascimento e somente o ano que deve ser colocado
